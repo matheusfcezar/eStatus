cf6144a0ceaa251dfd3b673a9325cd037e24004b
Vitor <vitorjv@gmail.com>
2019-03-15 20:45:13 -0300

    primeiro commit

---
+++ api/src/test/java/br/com/status/StatusApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
